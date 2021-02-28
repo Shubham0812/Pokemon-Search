39ff4a32db1988121916706c3aaca92e58dd2639
Shubham Singh <zero@Shubhams-MacBook-Pro.local>
2021-02-28 15:10:15 +0530

    Initialized Project

---
+++ Realm-Tutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Realm-Tutorial
+//
+//  Created by Shubham Singh on 28/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Realm-TutorialTests/Realm_TutorialTests.swift
@@ -0,0 +1,33 @@
+//
+//  Realm_TutorialTests.swift
+//  Realm-TutorialTests
+//
+//  Created by Shubham Singh on 28/02/21.
+//
+
+import XCTest
+@testable import Realm_Tutorial
+
+class Realm_TutorialTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
